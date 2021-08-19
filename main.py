8a8e38c8dab2da1c6cffad359db8cd9e2bbf6d2a
Heidi Wikman <Heidi.Wikman@sulava.com>
2021-08-19 22:59:40 +0300

    heroku test

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Jes tein ensimmäisen Flash web appini. Olen paras \o/"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
