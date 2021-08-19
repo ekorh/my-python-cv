466796b716a442f5fc9cfe2250f17efe36228785
Heidi Wikman <Heidi.Wikman@sulava.com>
2021-08-20 01:27:49 +0300

    final

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("cv.html")
+
+@app.route("/yhteystiedot")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
